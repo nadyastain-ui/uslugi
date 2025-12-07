daf18e2ca81349b2e293792a33de138ae892a0db
nadyastain-ui <nadyastain@gmail.com>
2025-12-07 22:39:20 +0300

    Add files via upload

---
+++ Мои-услуги-codebase/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer id="contact" className="py-16 border-t border-[#4F0005] bg-black">
+      <div className="container mx-auto px-6">
+        <div className="flex flex-col items-center gap-12">
+          <div className="text-center text-white/40 text-xs font-light">
+            <p className="!whitespace-pre-line">© 2025 Надя Михеева</p>
+          </div>
+        </div>
+      </div>
+    </footer>);
+
+}
